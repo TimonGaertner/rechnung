7a338940f06f5b982769dc144e3d13e41ab217d7
TimonGaertner <timon.ben.gaertner@gmail.com>
2022-04-01 01:07:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
+const fs = require("fs");
+const ipc = electron.ipcMain;
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadFile("html/index.html");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+var pdf = require("html-pdf");
+var options = { format: "Letter" };
+
+ipc.on("print-to-pdf", function (event) {
+    app.quit();
+});
